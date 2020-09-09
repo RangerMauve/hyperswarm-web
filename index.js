af271f22d46adc92a0661ed0b8e523d62e2d2a63
Martin Acosta <Martn Acosta>
2020-09-09 11:12:42 -0300

    Added sam signal

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const webRTCSwarm = require('@geut/discovery-swarm-webrtc')
 const HyperswarmClient = require('hyperswarm-proxy-ws/client')
 const DuplexPair = require('duplexpair')
 
-const DEFAULT_WEBRTC_BOOTSTRAP = ['wss://geut-webrtc-signal-v3.herokuapp.com', 'wss://geut-webrtc-signal-v3.glitch.me']
+const DEFAULT_WEBRTC_BOOTSTRAP = ['wss://geut-webrtc-signal-v3.herokuapp.com', 'wss://signal.dat-web.eu', 'wss://geut-webrtc-signal-v3.glitch.me']
 const DEFAULT_PROXY_SERVER = 'wss://hyperswarm.mauve.moe'
 
 module.exports = function swarm (opts) {
